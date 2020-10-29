0e4bbc774bf2d0baed9e2b13f45d1f21967f8f73
SomMariya <56650388+SomMariya@users.noreply.github.com>
2020-10-29 17:46:33 +0300

    Add files via upload

---
+++ lab3/js/main.js
@@ -0,0 +1,70 @@
+//1 task
+button.onclick = function ()
+{
+    swapBlock('header h3', '#cell-5')
+};
+// 2 task
+document.querySelector('#cell-4 h3').onclick = function ()
+{
+    const radius = 100;
+    document.querySelector('#last').textContent += ' S of circle  = ' + areaOfCircle(radius);
+};
+
+// 3 task
+document.querySelector('#form-min-digit').onsubmit = function (event)
+{
+    event.preventDefault();
+    let mindigit = min(document.querySelector('#form-min-digit > input[name="to-find-min-digit"]').value);
+    document.cookie = 'min-digit=' + mindigit;
+    document.querySelector('#form-min-digit').reset();
+    alert("Min digit = " + mindigit);
+}
+//window.addEventListener('load', function ()
+//{
+//    if (getCookie('min-digit')) {
+//        document.querySelector('#form-min-digit').style = "display:none;";
+//        setTimeout(() => {
+//            if (confirm(getCookie('min-digit') + "\nAfter you click \"OK\" cookies will be deleted !")) {
+//                document.cookie = 'min-digit=' + getCookie('min-digit') + '; max-age=0';
+//                alert('Cookies were removed !');
+//                location.reload()
+//            }
+//        }, 100);
+//    }
+//})
+
+//document.querySelector('header').onclick = function () {
+//    let radius = Number(prompt("Enter radius:"));
+//    if (radius) {
+//        if (radius > 0)
+//            document.querySelector('#last').textContent += ' S of circle  = ' + areaOfCircle(radius);
+//        else
+//            alert('Wrong value')
+//    }
+//    else {
+//        alert('Wrong value')
+//    }
+//};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 1 task
+function swapBlock(header, cell5) {
+    let temp = document.querySelector(header).innerHTML;
+    document.querySelector(header).innerHTML = document.querySelector(cell5).innerHTML;
+    document.querySelector(cell5).innerHTML = temp;
+}
+// 2 task
+function areaOfCircle(radius) {
+    return Math.PI * (radius * radius);
+}
+// 3 task
+function min(num) {
+    let n = num.split('');
+    return Math.min.apply(Math,n);
+}
+function getCookie(name) {
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++)
+        if (cookies[i].trim().split('=')[0] == name)
+            return cookies[i].trim().split('=')[1];
+    return null;
+}
